37649cffbf1824d044df6417560e7c96c3fa0844
VarunTyagarayanG <156878356+varunTyagarayanG@users.noreply.github.com>
2024-08-20 13:55:08 +0530

    feature/userBuyingOptions

--- controllers/user.js
+++ controllers/user.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const User = require("../models/user");
+const Car = require("../models/cars");
 
 exports.login = async (req, res) => {
   const { username, password } = req.body;
@@ -17,7 +18,6 @@ exports.login = async (req, res) => {
   }
 };
 
-
 exports.register = async (req, res) => {
   try {
     const newuser = new User(req.body);
@@ -25,5 +25,82 @@ exports.register = async (req, res) => {
     res.send("User registered successfully");
   } catch (error) {
     return res.status(400).json(error);
+  }
+};
+
+exports.getAvailableCars = async (req, res) => {
+  const { from, to } = req.query;
+
+  try {
+    const cars = await Car.find({
+      bookedTimeSlots: {
+        $not: {
+          $elemMatch: {
+            from: { $lt: to },
+            to: { $gt: from },
+          },
+        },
+      },
+    });
+
+    res.json(cars);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+exports.bookCar = async (req, res) => {
+  const { carId, from, to } = req.body;
+
+  try {
+    const car = await Car.findById(carId);
+
+    if (!car) {
+      return res.status(404).json({ message: "Car not found" });
+    }
+
+    const isBooked = car.bookedTimeSlots.some((slot) => {
+      return (
+        new Date(slot.from) < new Date(to) && new Date(slot.to) > new Date(from)
+      );
+    });
+
+    if (isBooked) {
+      return res
+        .status(400)
+        .json({
+          message: "Car is already booked for the specified time range",
+        });
+    }
+
+    car.bookedTimeSlots.push({ from, to });
+    await car.save();
+
+    res.json({ message: "Car booked successfully", car });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+exports.filterCars = async (req, res) => {
+  const { make, model, year, fuelType, minRent, maxRent } = req.query;
+
+  try {
+      const query = {};
+
+      if (make) query.make = make;
+      if (model) query.model = model;
+      if (year) query.year = year;
+      if (fuelType) query.fuelType = fuelType;
+      if (minRent || maxRent) {
+          query.rentPerHour = {};
+          if (minRent) query.rentPerHour.$gte = Number(minRent);
+          if (maxRent) query.rentPerHour.$lte = Number(maxRent);
+      }
+
+      const cars = await Car.find(query);
+      res.json(cars);
+  } catch (error) {
+      res.status(500).json({ message: error.message });
   }
 };
--- routes/user.js
+++ routes/user.js
@@ -1,8 +1,11 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const userControllers = require('../controllers/user');
+const userControllers = require("../controllers/user");
 
-router.post("/login", userControllers.login); 
-router.post("/register", userControllers.register); 
+router.post("/login", userControllers.login);
+router.post("/register", userControllers.register);
 
+router.get("/getAvailableCars", userControllers.getAvailableCars);
+router.post("/bookCar", userControllers.bookCar);
+router.post("/filterCars", userControllers.filterCars);
 module.exports = router;
